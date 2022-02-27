81cc14c7fe94c397d1113309ee41a6268304b9a5
EmanuelaMocci <emanuelamocci2@gmail.com>
2022-02-27 20:55:53 +0100

    Add: more backgorund

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
 const btnOn = document.getElementById("btn-on");
 const screen = document.getElementById("iphone-screen");
 const time = document.getElementById("time");
+const bg = document.getElementById("background-default");
+const btn1 = document.getElementById("background1");
+const btn2 = document.getElementById("background2");
 
 btnOn.addEventListener("click", function () {
   screen.classList.toggle("display-none");
 });
 
-let today = new Date();
+// set real time
+const date = Date().slice(16, 21);
 
-let getTime = today.getHours() + ":" + today.getMinutes();
-// getTime = ("0" + getTime).slice(-5);
+time.textContent = date;
 
-time.textContent = getTime;
+// background
+btn1.addEventListener("click", function () {
+  screen.classList.add("background1");
+  screen.classList.remove("background2");
+});
+btn2.addEventListener("click", function () {
+  screen.classList.add("background2");
+  screen.classList.remove("background1");
+});
+bg.addEventListener("click", function () {
+  screen.classList.remove("background1");
+  screen.classList.remove("background2");
+});
